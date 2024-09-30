ca594896257e6795ba970e7470ccaa4a2d46a58a
matternstsr <matternstsr@gmail.com>
2024-09-30 11:06:11 -0500

    size_t issues

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -49,7 +49,7 @@ void counting_sort(int *array, size_t size)
 	if (!end_array)
 		return;
 
-	for (i = 0; i <= (size_t)max_value; i++)
+	for (i = 0; i < size; i++)
 	{
 		while (counting_array[i] > 0)
 		{
