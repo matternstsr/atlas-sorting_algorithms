831eeb87b34dabc6092e29ed2879dc9e89dae5a0
matternstsr <matternstsr@gmail.com>
2024-09-30 09:30:42 -0500

    works locally but not sure thats what it wants yet..

---
+++ 100-shell_sort.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* 
+* print_array - Prints an array of integers.
+* @array: The array to print.
+* @size: The size of the array.
+*/
+void print_array(const int *array, size_t size)
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		printf("%d", array[i]);
+		if (i < size - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
+
+/* 
+* shell_sort - Sorts an array of integers using the Shell sort algorithm
+*               with the Knuth sequence.
+* @array: The array to sort.
+* @size: The size of the array.
+*/
+void shell_sort(int *array, size_t size)
+{
+	/* Sequence gaps */
+	size_t gap = 1;
+	size_t i;
+	
+	/* Figure out ate initial gap */
+	while (gap < size)
+		gap = gap * 3 + 1;
+
+	/* Start with the largest gap and then go down */
+	gap = (gap - 1) / 3;
+
+	while (gap > 0)
+	{
+		for (i = gap; i < size; i++)
+		{
+			int temp = array[i];
+			size_t j = i;
+
+			/* Sort with insertion using current gap */
+			while (j >= gap && array[j - gap] > temp)
+			{
+				array[j] = array[j - gap];
+				j -= gap;
+			}
+			array[j] = temp;
+		}
+
+		/* Print the array after each itteration */
+		print_array(array, size);
+		
+		/* Figue out the next gap */
+		gap = (gap - 1) / 3;
+	}
+}
