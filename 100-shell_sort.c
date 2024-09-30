fb839ce4d30dd6e40baaeec7bef9b158652131e7
matternstsr <matternstsr@gmail.com>
2024-09-30 09:34:43 -0500

    works locally but not sure thats what it wants yet..

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -2,24 +2,6 @@
 #include <stdlib.h>
 
 /* 
-* print_array - Prints an array of integers.
-* @array: The array to print.
-* @size: The size of the array.
-*/
-void print_array(const int *array, size_t size)
-{
-	size_t i;
-
-	for (i = 0; i < size; i++)
-	{
-		printf("%d", array[i]);
-		if (i < size - 1)
-			printf(", ");
-	}
-	printf("\n");
-}
-
-/* 
 * shell_sort - Sorts an array of integers using the Shell sort algorithm
 *               with the Knuth sequence.
 * @array: The array to sort.
